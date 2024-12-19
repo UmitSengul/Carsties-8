5c1fa0555a00d97d72baf6aa3d308d720fa8e062
Umit Sengül <umitsengul.de@gmail.com>
2024-12-19 10:26:27 +0300

    End of Section 5

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -23,8 +23,8 @@ builder.Services.AddMassTransit(x =>
     {
         cfg.ReceiveEndpoint("search-auction-created", e =>
         {
-            e.UseMessageRetry(r => r.Interval(5,5));
-            
+            e.UseMessageRetry(r => r.Interval(5, 5));
+
             e.ConfigureConsumer<AuctionCreatedConsumer>(context);
         });
 
